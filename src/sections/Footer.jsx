65fb086056a021717932b7a248bed9318cd30a4d
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2025-08-21 01:51:48 +0530

    Updated package dependencies and file contents to reflect changes in project metadata and code.

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -9,17 +9,23 @@ const Footer = () => {
 
       <div className="flex gap-3">
         <div className="social-icon">
-          <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+          <a href="https://github.com/pbbhattpriyanshu" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/github.svg" alt="github" className="w-8 h-8" />
+          </a>
         </div>
         <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+          <a href="https://x.com/Priyanshu12bha" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/twitter.svg" alt="twitter" className="w-8 h-8" />
+          </a>
         </div>
         <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+          <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
+            <img src="/assets/linkedin.png" alt="linkedin" className="w-8 h-8" />
+          </a>
         </div>
       </div>
 
-      <p className="text-white-500">© 2024 Adrian Hajdin. All rights reserved.</p>
+      <p className="text-white-500">© 2025 Priyanshu Bhatt. All rights reserved.</p>
     </footer>
   );
 };
