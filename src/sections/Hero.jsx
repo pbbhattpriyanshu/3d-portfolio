3f02ce3e9e057bffb0e825e6347c5b6cfd2d1f74
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2025-08-29 14:14:09 +0530

    Refactor CSS classes and values in Contact and Hero components

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
-    <section className="min-h-screen w-full flex flex-col relative" id="home">
+    <section className="min-h-96 w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Priyanshu <span className="waving-hand">👋</span>
@@ -26,7 +26,7 @@ const Hero = () => {
         <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
       </div>
 
-      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+      <div className="absolute bottom-4 left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
